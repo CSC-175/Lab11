7049916cb33d636d4fec4d24228356c937ba8c3b
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2023-10-02 19:36:09 -0400

    Update Lab11.cpp

--- Lab11.cpp
+++ Lab11.cpp
@@ -1,7 +1,7 @@
-// Write a program that prompts the user to enter an integer 
-// to define the number of rows to be printed for a right triangle 
-// then use nested loops to iteratively print a pattern of 
-// asterisks to form a right triangle pattern.
+// A prime number is an integer number greater than 1 that has no positive divisors other than one and itself.
+// Write a program that will prompt the user for an integer n that will be tested to determine if it is a prime number. 
+// The best approach is to start by assuming the integer n is a prime number (isPrime=true) and attempting to prove 
+// it is not prime by testing it to find a number i that divides it evenly using the modulo (remainder) operator (i.e.  n%i==0 ).
 
 #include <iostream>
 using namespace std;

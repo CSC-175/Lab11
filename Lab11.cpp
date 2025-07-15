7c76bbfa5f03b73198a3772ae8b582a0293e25c0
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2025-07-15 14:08:32 -0400

    Initial Commit

--- Lab11.cpp
+++ Lab11.cpp
@@ -1,12 +1,8 @@
-// A prime number is an integer number greater than 1 that has no positive divisors other than one and itself.
-// Write a program that will prompt the user for an integer n that will be tested to determine if it is a prime number. 
-// The best approach is to start by assuming the integer n is a prime number (isPrime=true) and attempting to prove 
-// it is not prime by testing it to find a number i that divides it evenly using the modulo (remainder) operator (i.e.  n%i==0 ).
-
 #include <iostream>
 using namespace std;
 
 int main() {
+    // Place your code for the Prime Number Finder program here
 
     return 0;
 }
